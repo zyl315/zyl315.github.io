2aeb841fe51fe4b8ea1ba9084251d33ed5d976a1
zyl315 <649587981@qq.com>
2020-10-18 21:41:22 +0800

    Site updated: 2020-10-18 21:41:07

---
+++ demo/CSS3/js/ac.js
@@ -0,0 +1,25 @@
+function hide() {
+    document.getElementById('header').style.display = 'none';
+    document.getElementById('footer').style.display = 'none';
+    document.getElementsByClassName('list-container')[0].style.display = 'none';
+}
+
+function clickFilmModel() {
+    document.getElementsByClassName('btn-film-model')[0].click();
+}
+
+function clickFullscreen() {
+    document.getElementsByClassName('btn-fullscree')[0].click();
+}
+
+function clickPlay() {
+    document.getElementsByClassName('btn-play')[0].click();
+}
+
+function clickRefresh() {
+    document.getElementsByClassName('btn-play')[0].click();
+}
+
+function clickVolume() {
+    document.getElementsByClassName('volume-icon')[0].click();
+}
